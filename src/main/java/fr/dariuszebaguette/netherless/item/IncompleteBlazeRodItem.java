bd4fa4be54dc83013e3401dd7516d17fab9c6bef
DariusZeBaguette <dariu@DESKTOP-SFOKPVR>
2026-04-19 21:12:30 +0200

    General removal of ectoplasm (still in game but deprecated), fix of a recipe causing errors in logs, 'recipes' folder was renamed to 'recipe' but the recipes are still not working and the incomplete blaze rod now extends SequencedAssemblyItem.

--- src/main/java/fr/dariuszebaguette/netherless/item/IncompleteBlazeRodItem.java
+++ src/main/java/fr/dariuszebaguette/netherless/item/IncompleteBlazeRodItem.java
@@ -4,7 +4,9 @@ package fr.dariuszebaguette.netherless.item;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.Item;
 
-public class IncompleteBlazeRodItem extends Item {
+import com.simibubi.create.content.processing.sequenced.SequencedAssemblyItem;
+
+public class IncompleteBlazeRodItem extends SequencedAssemblyItem {
 	public IncompleteBlazeRodItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
 	}
