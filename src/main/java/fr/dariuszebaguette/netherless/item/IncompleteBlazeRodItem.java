5130a1dd04b112ce4c1c132a07f54edbd4f412c9
DariusZeBaguette <dariu@DESKTOP-SFOKPVR>
2026-04-20 16:50:09 +0200

    Oops, little brainfart on my side, forgot to do some imports, THIS should be the final 2.0.0 version

--- src/main/java/fr/dariuszebaguette/netherless/item/IncompleteBlazeRodItem.java
+++ src/main/java/fr/dariuszebaguette/netherless/item/IncompleteBlazeRodItem.java
@@ -1,7 +1,12 @@
 
 package fr.dariuszebaguette.netherless.item;
 
-public class IncompleteBlazeRodItem extends Item {
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+import com.simibubi.create.content.processing.sequenced.SequencedAssemblyItem;
+
+public class IncompleteBlazeRodItem extends SequencedAssemblyItem {
 	public IncompleteBlazeRodItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
 	}
