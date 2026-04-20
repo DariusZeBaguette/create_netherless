9b985cbf06e2b9b57811aceb6c7521c84ae6681a
dariu <dariu@DESKTOP-SFOKPVR>
2026-04-20 16:53:46 +0200

    MCreator Merge commit '234b2326a08f26cbd5fd7a2bf87a9630e40bd980' into 5dbbda4d9aa30f7d158d22f209d2eec7d62ae70c

--- src/main/java/fr/dariuszebaguette/netherless/item/IncompleteBlazeRodItem.java
+++ src/main/java/fr/dariuszebaguette/netherless/item/IncompleteBlazeRodItem.java
@@ -1,12 +1,7 @@
 
 package fr.dariuszebaguette.netherless.item;
 
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.Item;
-
-import com.simibubi.create.content.processing.sequenced.SequencedAssemblyItem;
-
-public class IncompleteBlazeRodItem extends SequencedAssemblyItem {
+public class IncompleteBlazeRodItem extends Item {
 	public IncompleteBlazeRodItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
 	}
